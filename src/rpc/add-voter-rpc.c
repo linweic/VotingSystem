3055342805169765c32af24b6da2000e34d4c699
Linwei Chen <linweic@users.noreply.github.com>
2016-03-13 15:09:50 -0400

    formatting add-voter-rpc

--- src/rpc/add-voter-rpc.c
+++ src/rpc/add-voter-rpc.c
@@ -15,17 +15,17 @@ int main(int argc, char *argv[])
 	//*voterid = argv[2];
 	cl = clnt_create(argv[1], VOTINGSYS, VOTINGSYS_V1, "tcp");
 	if (cl == NULL) {
-        printf("error: could not connect to server.\n");
+		printf("error: could not connect to server.\n");
         return 1;
-    }
-    printf("Client connects to server successfully.\n");
+	}
+	printf("Client connects to server successfully.\n");
 	
 	//call remote procedure
 	result = addvoter_1(&argv[2], cl);
 	if (result == NULL) {
-        clnt_perror(cl,argv[1]);
+		clnt_perror(cl,argv[1]);
         exit(1);
-    }
+	}
     printf("%s\n", *result);
 	return 0;
 }
